32011013f12c693ccb47e13bb8a78e574de6a164
User2 <User2@otsuka-PC>
2018-08-17 18:42:54 +0900

    Cybertech 社内研修
    変更した内容
    ・漫画情報登録画面の実装（途中）

--- src/servlet/ComicInfoRegistrationServlet.java
+++ src/servlet/ComicInfoRegistrationServlet.java
@@ -37,9 +37,6 @@ import service.ComicService;
 public class ComicInfoRegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
